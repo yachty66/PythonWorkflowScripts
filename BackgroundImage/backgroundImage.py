3405114b6c9bf335498fb717ee78ac19a295d3c3
Max Hager <maxhager@Maxs-Air.fritz.box>
2022-08-18 09:30:32 +0200

    s

---
+++ BackgroundImage/backgroundImage.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+def getFilename():
+    r = requests.get(url)
+    path = "/Users/maxhager/Projects2022/PythonWorkflowScripts/BackgroundImage/backgroundImage/"
+    titleUrl = r.json()['title'].replace(" ", "")
+    filename = path + titleUrl + ".png"
+    return filename
+
+
+def downloadImageToday():
+    r = requests.get(url)
+    if r.status_code != 200:
+        print('error')
+        return
+    pictureUrl = r.json()['url']
+    if "jpg" not in pictureUrl:
+        print("No image for today, must be a video")
+    else:
+        pic = requests.get(pictureUrl, allow_redirects=True)
+        filename = getFilename()
+        open(filename, 'wb').write(pic.content)
+        print(f"saved picture of the day to {filename}!")
+
+
+if __name__ == '__main__':
+    downloadImageToday()
+    filename = getFilename()
+    cmd = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" + filename + "\"'"
+    os.system(cmd)
+    
